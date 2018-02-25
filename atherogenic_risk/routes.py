680bb4d23a7d38cf4392dca9bbbfcfda3d86adfd
Mensur Mandzuka <mensur.mandzuka@gmail.com>
2018-02-25 17:19:12 +0100

    initial commit

---
+++ atherogenic_risk/routes.py
@@ -0,0 +1,18 @@
+from atherogenic_risk import app
+from flask import render_template, request, redirect
+
+
+@app.route('/')
+def index():
+    return redirect('/first-step')
+
+@app.route('/first-step', methods=['GET', 'POST'])
+def first_step():
+    if request.method == 'GET':
+        return render_template('first-step.html')
+
+    tc = request.form['cholesterol']
+    tg = request.form['triglyceride']
+    hdl = request.form['lipoproteins']
+
+    return render_template('second-step.html')
---
+++ run.py
@@ -0,0 +1,4 @@
+from atherogenic_risk import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
